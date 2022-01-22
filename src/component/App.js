a0a176da7e071e989a02e625c05ad3c9c81502e6
vinay <vinaypn16@gmail.com>
2022-01-22 16:59:10 +0530

    initial project setup

---
+++ src/component/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+	return (
+		<div className="row">
+			<div className="col-md-12 d-flex justify-content-center mt-5">
+				<h2>App component.</h2>
+			</div>
+		</div>
+	);
+};
+
+export default App;

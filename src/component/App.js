4139ba581a3db45a52dd6a636d4549b8bab6cb6c
vinay <vinaypn16@gmail.com>
2022-01-23 20:43:14 +0530

    usestate and useeffect added

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,14 @@
+import ClassComponent from "./ClassComponent";
+import UseStateComponent from "./useState/UseStateComponent";
+import UseEffectComponent from "./useEffect/UseEffectComponent";
+
 const App = () => {
 	return (
 		<div className="row">
-			<div className="col-md-12 d-flex justify-content-center mt-5">
-				<h2>App component.</h2>
+			<div className="col-md-12 d-flex flex-column justify-content-center align-items-center mt-5">
+				{/* <ClassComponent></ClassComponent> */}
+				{/* <UseStateComponent></UseStateComponent> */}
+				{/* <UseEffectComponent></UseEffectComponent> */}
 			</div>
 		</div>
 	);
